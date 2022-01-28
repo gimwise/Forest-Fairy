776ecaec36a461aebb34383000de07269b0830f3
Minsol Kim <artkms4815@naver.com>
2022-01-29 00:10:53 +0900

    nav url setting

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,10 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 
 app_name = 'home'
 
 urlpatterns = [
-
+    path('',views.home,name='home'),
+    path('add/',views.add, name='add'),
 ]
