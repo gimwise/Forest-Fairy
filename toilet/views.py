03c7bcfe9b055f29ae4790e65b2e670133320536
juoonge <juoonge7646@naver.com>
2022-01-30 00:26:04 +0900

    add toilet data

--- toilet/views.py
+++ toilet/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def home(request):
     return render(request, 'home.html')
+
 
 def add(request):
     return render(request, 'toilet/add.html')
