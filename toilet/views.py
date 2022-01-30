28d70204dc149b177cf6c905c9407f704a00252d
김지혜 <jaejae@gimjihyeui-MacBookAir.local>
2022-01-31 05:28:33 +0900

    남은 연휴동안 저를 찾지마세요,,

--- toilet/views.py
+++ toilet/views.py
@@ -1,15 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import ToiletInfo
+from rest_framework import viewsets
+from .serializers import ToiletSerializer
 
 # Create your views here.
 
-def home(req):
-    return render(req, 'home.html')
+class ToiletViewSet(viewsets.ModelViewSet):
+    queryset = ToiletInfo.objects.all()
+    serializer_class = ToiletSerializer
+
+def home(request):
+    toilet_list = ToiletInfo.objects.all()
+    # toilet_list = ToiletInfo.objects.order_by('-id')
+    context = {'toilet_list' : toilet_list}
+    return render(request, 'home.html', context)
 
 def add(req):
     return render(req, 'toilet/add.html')
 
-def info(req):
-    return render(req, 'toilet/info.html')
+def info(request, id):
+    toilet = get_object_or_404(ToiletInfo, pk=id)
+    context = {'toilet' : toilet}
+    return render(request, 'toilet/info.html', context)
 
 def intro(req):
     return render(req, 'toilet/intro.html')
--- users/admin.py
+++ users/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from .models import User
 
 # Register your models here.
-admin.site.register(User)
+
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ('username', 'email')
+    list_display = ('username', 'email', 'is_superuser')
+
+admin.site.register(User, UserAdmin)
