bf10b0035c9f856ac33034afe0a12bd2147a5ea2
김지혜 <jaejae@gimjihyeui-MacBookAir.local>
2022-02-02 18:26:22 +0900

    화장실 데이터 불러오는 방법 수정

--- toilet/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import ToiletInfo
-
-class ToiletSerializer(serializers.ModelSerializer):
-    class Meta :
-        model = ToiletInfo
-        fields = ('id','tname', 'tlocation', 'tlat', 'tlong')
