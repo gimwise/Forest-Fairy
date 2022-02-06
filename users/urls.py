77a220c986051abfa7c83a118c010ce3b9d66ae8
Minsol Kim <artkms4815@naver.com>
2022-02-07 00:59:34 +0900

    profiles css

--- users/urls.py
+++ users/urls.py
@@ -9,6 +9,8 @@ app_name = 'users'
 
 urlpatterns = [
     path("profile/<str:id>/", views.profile, name="profile"),
+    path("editProfile/<str:id>/", views.editProfile, name="editProfile"),
+        path("editPassword/<str:id>/", views.editPassword, name="editPassword"),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('join/', views.join, name='join'),
