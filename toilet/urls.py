34b93484897778e91f1a205613a16bc751ba2229
GimWise <77537852+gimwise@users.noreply.github.com>
2022-02-14 20:53:49 +0900

    DB 살려

--- toilet/urls.py
+++ toilet/urls.py
@@ -5,26 +5,16 @@ from . import views
 app_name = 'toilet'
 
 urlpatterns = [
-<<<<<<< HEAD
-    path('', views.home, name='home'),
-    path('add/', views.add, name='add'),
-    path('<int:id>/edit/', views.edit, name="edit"),
-    path('<int:id>/', views.info, name='info'),
-=======
     path('',views.home,name='home'),
     path('admin/', admin.site.urls),
     path('add/',views.add, name='add'),
     path('<int:id>/',views.info, name='info'),
->>>>>>> real/main
     path("intro/", views.intro, name="intro"),
     path('addBookmark/<int:toilet_id>/', views.addBookmark, name="addBookmark"),
     path('delBookmark/<int:toilet_id>/', views.delBookmark, name="delBookmark"),
     path('bookmarks/<str:id>/', views.bookmarks, name="bookmarks"),
     path('getJson/', views.getJson, name='getJson'),
     path('getScore/', views.getScore, name='getScore'),
-<<<<<<< HEAD
+    path('myComments/', views.myComments, name='myComments'),
 ]
-=======
-    path('myComments/', views.myComments, name='myComments')
-]
->>>>>>> real/main
+
