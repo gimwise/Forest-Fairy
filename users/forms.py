32f1a31c362df378e9c32b75a150d65b10759b48
GimWise <77537852+gimwise@users.noreply.github.com>
2022-02-15 00:49:27 +0900

    프로필 수정 / 비밀번호 수정 기능 및 CSS

--- users/forms.py
+++ users/forms.py
@@ -65,4 +65,41 @@ class UserForm(UserCreationForm):
             'birthdate', 'gender',
         ]
 
+class CustomPasswordChangeForm(PasswordChangeForm):
+    def clean_new_password1(self):
+        old_password = self.cleaned_data.get('old_password')
+        new_password1 = self.cleaned_data.get('new_password1')
 
+        if old_password and new_password1 :
+            if old_password == new_password1:
+                raise forms.ValidationError('새로운 암호는 기존 암호와 다르게 입력해주세요.')
+        return new_password1
+
+    def __init__(self, *args, **kwargs):
+        super(CustomPasswordChangeForm, self).__init__(*args, **kwargs)
+        self.fields['old_password'].label = '기존 비밀번호'
+        self.fields['old_password'].widget.attrs.update({
+            'class': 'pw',
+            'autofocus': False,
+            'placeholder' : '기존 비밀번호',
+        })
+        self.fields['new_password1'].label = '새 비밀번호'
+        self.fields['new_password1'].widget.attrs.update({
+            'class': 'pw',
+            'placeholder' : '새 비밀번호',
+        })
+        self.fields['new_password2'].label = '새 비밀번호 확인'
+        self.fields['new_password2'].widget.attrs.update({
+            'class': 'pw',
+            'placeholder' : '새 비밀번호 확인',
+        })
+
+class CustomUserChangeForm(UserChangeForm):
+    password = None
+    class Meta:
+        model = get_user_model()
+        fields = [
+            'nickname',
+            'gender',
+            'email',
+        ]
--- users/views.py
+++ users/views.py
@@ -1,11 +1,24 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth import authenticate, login, get_user_model
-from users.forms import UserForm
+from django.contrib.auth import authenticate, login, get_user_model, update_session_auth_hash
+from users.forms import UserForm, CustomPasswordChangeForm, CustomUserChangeForm
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import PasswordChangeView as AuthPasswordChangeView
+from django.urls import reverse_lazy
 from users.models import *
 from toilet.models import Bookmarks
 
+class PasswordChangeView(LoginRequiredMixin, AuthPasswordChangeView):
+    success_url = reverse_lazy('editPassword')
+    template_name =  'users/editPassword.html'
+    form_class = CustomPasswordChangeForm
+
+    def form_valid(self, form):
+        messages.success(self.request, '암호를 변경하였습니다.')
+        return super().form_valid(form)
 
 # Create your views here.
 @login_required
@@ -17,21 +30,18 @@ def profile(request, id):
     }
     return render(request, 'users/profile.html', context)
 
-def editProfile(request, id):
-    User = get_user_model()
-    user = get_object_or_404(User, pk=id)
-    context = {
-        'user' :user
-    }
-    return render(request, 'users/editProfile.html', context)
+@login_required
+def editProfile(request):
+    if request.method == 'POST':
+        form = CustomUserChangeForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+        return redirect('/')
+    else:
+        form = CustomUserChangeForm(instance=request.user)
+        return render(request, 'users/editProfile.html', {'form': form})
 
-def editPassword(request, id):
-    User = get_user_model()
-    user = get_object_or_404(User, pk=id)
-    context = {
-        'user' :user
-    }
-    return render(request, 'users/editPassword.html', context)
+editPassword = PasswordChangeView.as_view()
 
 @csrf_exempt
 def join(request):
